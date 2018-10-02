6ce2dff08c2e3706dde36e8cd6cbe7e2feee05f9
James Livermont <j_livermont@yahoo.com>
2018-10-02 01:09:34 -0500

    Added some settings and the station model

--- wapi/ingest/models.py
+++ wapi/ingest/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Station(models.Model):
+    station_id = models.CharField(max_length=64, unique=True, editable=False)
+    name = models.CharField(max_length=200)
+    min_date = models.DateField()
+    max_date = models.DateField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    data_coverage = models.FloatField()
+    elevation = models.FloatField()
+    elevation_unit = models.CharField(max_length=24)
+    created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return "{0} [ID: {1}]".format(self.name, self.station_id)
